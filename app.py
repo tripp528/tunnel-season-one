e1204687be8e638b9e98c718cc1e79051ab9b77b
Tripp Gordon <jgordo17@uvm.edu>
2020-12-24 22:23:47 -0700

    got react flask chat app local running

--- app.py
+++ app.py
@@ -1,51 +1,25 @@
 # app.py
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_socketio import SocketIO, send
+
+# create app
 app = Flask(__name__)
+# add secret key
+app.config['SECRET_KEY'] = 'mysecret'
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+# create server
+socket = SocketIO(app, cors_allowed_origins="*")
 
-    # For debugging
-    print(f"got name {name}")
+# makes it so you don't need to restart on changing code
+app.debug = True
 
-    response = {}
+# called when client emits "message" event
+@socket.on("message")
+def handle_message(msg):
+    print("message event handler called in app.py", msg)
+    send(msg, broadcast=True)
+    return None
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
-
+# run the app
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    socket.run(app)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
 
-function App() {
+let endPoint = "http://localhost:5000";
+let socket = io.connect(`${endPoint}`);
+
+const App = () => {
+  const [messages, setMessages] = useState(["Hello And Welcome"]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    getMessages();
+  }, [messages.length]);
+
+  const getMessages = () => {
+    socket.on("message", msg => {
+      //   let allMessages = messages;
+      //   allMessages.push(msg);
+      //   setMessages(allMessages);
+      setMessages([...messages, msg]);
+    });
+  };
+
+  // On Change
+  const onChange = e => {
+    setMessage(e.target.value);
+  };
+
+  // On Click
+  const onClick = () => {
+    if (message !== "") {
+      socket.emit("message", message);
+      setMessage("");
+    } else {
+      alert("Please Add A Message");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {messages.length > 0 &&
+        messages.map(msg => (
+          <div>
+            <p>{msg}</p>
+          </div>
+        ))}
+      <input value={message} name="message" onChange={e => onChange(e)} />
+      <button onClick={() => onClick()}>Send Message</button>
     </div>
   );
-}
+};
 
 export default App;
