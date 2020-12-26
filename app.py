885bb408017f28c8318345df6e7fc87a54a1d278
Tripp Gordon <jgordo17@uvm.edu>
2020-12-25 23:03:11 -0700

    database setup

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import os
 from flask import Flask, send_from_directory
 from flask_socketio import SocketIO, send
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
 
 # create app
 app = Flask(__name__, static_folder='./build')
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 # create server
 socket = SocketIO(app, cors_allowed_origins="*")
@@ -25,7 +31,9 @@ def serve(path):
     else:
         return send_from_directory(app.static_folder, 'index.html')
 
-        
+# migrations 
+migrate = Migrate(app, db)
+
 # run the app
 if __name__ == '__main__':
     print(os.environ['APP_SETTINGS'])
